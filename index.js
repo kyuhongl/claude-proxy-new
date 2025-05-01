281dcc09cc83f2d9dedf3d0d21be4175913364aa
Kyuhong Lee <kyuhongl@usc.edu>
2025-04-30 20:38:19 -0700

    hi

--- index.js
+++ index.js
@@ -37,5 +37,6 @@ app.post('/ask', async (req, res) => {
 });
 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
-
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
