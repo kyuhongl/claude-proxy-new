2aa01025005eda2d2411a14cd382ad809811f4f7
Kyuhong Lee <kyuhongl@usc.edu>
2025-04-30 21:01:27 -0700

    fetch issue

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
-const fetch = require('node-fetch');
-
+const fetch = (...args) => import('node-fetch').then(({default: fetch}) => fetch(...args));
 const app = express();
 app.use(cors({
   origin: '*', // or replace with your extension ID if you want to restrict
